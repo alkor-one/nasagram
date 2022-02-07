cd249300e0e1d007a6381bde3fe59f14a3d576d7
alkor-one <ikorotkoff@gmail.com>
2022-02-06 20:52:03 -0500

    Clean code switch pages method in pagination component

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -29,14 +29,10 @@ export class PaginationComponent implements OnChanges {
     this.sendPageToParent.emit(page);
   }
 
+
   // Switch page numbers if the page number goes above or below than the middle page number of the row
   public switchPages(page: number): void {
-    if (page === 1 && this.viewPortWidth > 650){
-      this.pageLimitIndexStart = 0;
-      this.pageLimitIndexEnd = 10;
-    } else if (page === 1 && this.viewPortWidth < 650) {
-      this.pageLimitIndexEnd = 3;
-    }
+    this.setPageNumbersByDevice(page);
     if (this.pageLimitIndexEnd > this.totalPages.length) {
       this.pageLimitIndexEnd = this.totalPages.length;
     }
@@ -44,22 +40,39 @@ export class PaginationComponent implements OnChanges {
       (this.pageLimitIndexEnd + this.pageLimitIndexStart + 1) / 2,
     );
     if (page > middleOfPages && this.pageLimitIndexEnd !== this.totalPages.length) {
-      if (this.pageLimitIndexEnd + (page - middleOfPages) > this.totalPages.length) {
-        this.pageLimitIndexStart =
-          this.pageLimitIndexStart + (this.totalPages.length - this.pageLimitIndexEnd);
-        this.pageLimitIndexEnd = this.totalPages.length;
-      } else {
-        this.pageLimitIndexStart = this.pageLimitIndexStart + (page - middleOfPages);
-        this.pageLimitIndexEnd = this.pageLimitIndexEnd + (page - middleOfPages);
-      }
+      this.increasePageNumbers(page, middleOfPages);
     } else if (page < middleOfPages && this.pageLimitIndexStart !== 0) {
-      if (this.pageLimitIndexStart - (middleOfPages - page) < 0) {
-        this.pageLimitIndexEnd = this.pageLimitIndexEnd - this.pageLimitIndexStart;
-        this.pageLimitIndexStart = 0;
-      } else {
-        this.pageLimitIndexStart = this.pageLimitIndexStart - (middleOfPages - page);
-        this.pageLimitIndexEnd = this.pageLimitIndexEnd - (middleOfPages - page);
-      }
+      this.decreasePageNumbers(middleOfPages, page);
+    }
+  }
+
+  private setPageNumbersByDevice(page: number) {
+    if (page === 1 && this.viewPortWidth > 650) {
+      this.pageLimitIndexStart = 0;
+      this.pageLimitIndexEnd = 10;
+    } else if (page === 1 && this.viewPortWidth < 650) {
+      this.pageLimitIndexEnd = 3;
+    }
+  }
+
+  private increasePageNumbers(page: number, middleOfPages: number) {
+    if (this.pageLimitIndexEnd + (page - middleOfPages) > this.totalPages.length) {
+      this.pageLimitIndexStart =
+        this.pageLimitIndexStart + (this.totalPages.length - this.pageLimitIndexEnd);
+      this.pageLimitIndexEnd = this.totalPages.length;
+    } else {
+      this.pageLimitIndexStart = this.pageLimitIndexStart + (page - middleOfPages);
+      this.pageLimitIndexEnd = this.pageLimitIndexEnd + (page - middleOfPages);
+    }
+  }
+
+  private decreasePageNumbers(middleOfPages: number, page: number) {
+    if (this.pageLimitIndexStart - (middleOfPages - page) < 0) {
+      this.pageLimitIndexEnd = this.pageLimitIndexEnd - this.pageLimitIndexStart;
+      this.pageLimitIndexStart = 0;
+    } else {
+      this.pageLimitIndexStart = this.pageLimitIndexStart - (middleOfPages - page);
+      this.pageLimitIndexEnd = this.pageLimitIndexEnd - (middleOfPages - page);
     }
   }
 
